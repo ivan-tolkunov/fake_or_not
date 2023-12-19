f49832bf19b4dd0041c744be207cb340171d28cc
Ivan Tolkunov <tolkunov.ivan.s@gmail.com>
2023-12-19 14:54:58 -0800

    Refactored model.py

--- model.py
+++ model.py
@@ -1,15 +1,22 @@
 from fastai.vision.all import *
 
-path = Path('data')
+class FakeOrNotModel:
 
-dls = DataBlock(
-  blocks=(ImageBlock, CategoryBlock), 
-  get_items=get_image_files, 
-  splitter=RandomSplitter(valid_pct=0.2, seed=42),
-  get_y=parent_label
-).dataloaders(path, bs=32)
+  def __init__(self, path = Path('data/train_set')):
+    self.path = Path('data')
 
-learn = vision_learner(dls, resnet18, metrics=error_rate)
-learn.fine_tune(5)
+  def tain(self):
+    dls = DataBlock(
+      blocks=(ImageBlock, CategoryBlock), 
+      get_items=get_image_files, 
+      splitter=RandomSplitter(valid_pct=0.2, seed=42),
+      get_y=parent_label
+    ).dataloaders(self.path, bs=32)
 
-learn.export('./models/realOrFake_resnet18_224px.pkl')
+    learn = vision_learner(dls, resnet18, metrics=error_rate)
+    learn.fine_tune(5)
+
+    learn.export('./models/realOrFake_resnet18_224px.pkl')
+
+  def get_model(self):
+    return load_learner('./models/realOrFake_resnet18_224px.pkl')
